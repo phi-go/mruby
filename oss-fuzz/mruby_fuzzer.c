4022cca42e5e4e3b8ff5297e459003198797046f
phi-go <github@philipp-goerz.com>
2025-01-31 14:10:18 +0100

    bla

--- oss-fuzz/mruby_fuzzer.c
+++ oss-fuzz/mruby_fuzzer.c
@@ -1,12 +1,36 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <mruby.h>
 #include <mruby/compile.h>
+
+static int saved_null;
+static int saved_stdout;
+static int saved_stderr;
+
+int LLVMFuzzerInitialize(int *argc, char ***argv) {
+    saved_null = open("/dev/null", O_WRONLY);
+    saved_stdout = dup(STDOUT_FILENO);
+    saved_stderr = dup(STDERR_FILENO);
+    return 0;
+}
 
 int LLVMFuzzerTestOneInput(uint8_t *Data, size_t size) {
     if (size < 1) {
         return 0;
     }
+
+    dup2(saved_null, STDOUT_FILENO);
+    dup2(saved_null, STDERR_FILENO);
+    if (freopen("/dev/null", "w", stdout) == NULL) {
+        return 0;
+    }
+    if (freopen("/dev/null", "w", stderr) == NULL) {
+        return 0;
+    }
+
     char *code = malloc(size+1);
     memcpy(code, Data, size);
     code[size] = '\0';
@@ -14,5 +38,10 @@ int LLVMFuzzerTestOneInput(uint8_t *Data, size_t size) {
     mrb_load_string(mrb, code);
     mrb_close(mrb);
     free(code);
+
+    fflush(stdout);
+    fflush(stderr);
+    dup2(saved_stdout, STDOUT_FILENO);
+    dup2(saved_stderr, STDERR_FILENO);
     return 0;
 }
